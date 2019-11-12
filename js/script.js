5d6cd2bf90888069073b0e21ff709b702b9e872b
Jonas Pedersen <jonasmp1997@gmail.com>
2019-11-12 17:26:36 +0100

    tilføjet login requireries  med JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // Javascript document
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "simonkarlsson@live.dk" && password == "simon1234"){
+
+window.location = "key.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("Du har "+attempt+" forsøg tilbage");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
